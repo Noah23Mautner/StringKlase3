905b54ef97a6f4281c9186082768dc40fe98d83d
Noah23Mautner <152974783+Noah23Mautner@users.noreply.github.com>
2024-01-16 12:24:58 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringKlase2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1;
+
+            Console.WriteLine("Upisi recenicu");
+            s1=Console.ReadLine();
+
+            string zamjenznakova = s1.Replace(" ","_");
+            Console.WriteLine(zamjenznakova);
+
+            Console.ReadKey();
+        }
+    }
+}
